07ba7324dded88d7b9359d316dccb696b9672cd7
diegopq <d.padillaquijas@gmail.com>
2023-05-07 19:52:08 -0600

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Diego Padilla Quijas on 07/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
